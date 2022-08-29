5557f44f59946026ffb34525d4d1fb837149b804
Usman Murtala <uzlash16@gmail.com>
2022-08-29 17:33:17 +0100

    quick modifications

--- src/Faq.jsx
+++ src/Faq.jsx
@@ -4,13 +4,13 @@ import Faqbox from './Faqbox';
 import Navbar from './Navbar';
 
 const Faq = () => {
- const [faqData, setfaqData] = useState(accordionData);
+ const [faqData] = useState(accordionData);
   return (
     <>
     <section className='staking'>
       <Navbar/>
-      <div className=' faq-container col-lg-8 col-md-10 col-11 mx-auto text-center'>
-      <h1>FAQs</h1>
+      <div className='faq-container col-lg-8 col-md-10 col-11 mx-auto text-center'>
+      <h2 className='h2'>FAQs</h2>
     { 
       faqData.map((ce) =>{
         const {id} = ce;
